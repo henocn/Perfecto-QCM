e2c48693493a2678969f0fe7bf147b7a69b2104a
Henoc N'GASAMA <ngasamah@gmail.com>
2025-12-23 18:01:27 +0000

    return on the web version instead of reste API

--- qcm/settings.py
+++ qcm/settings.py
@@ -21,7 +21,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main',
-    'rest_framework',
+    # 'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
